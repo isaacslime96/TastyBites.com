13b3cf3fa07bdfb973e69225ebc086e52491a50c
Isaac Slime <115994303+isaacslime96@users.noreply.github.com>
2023-08-05 21:44:41 +0100

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+const navbar= document.querySelector('.navbar')
+
+window.addEventListener('scroll', () => {
+    if (this.scrollY > 0) {
+        navbar.classList.remove('py-2')
+        navbar.classList.add('shadow', 'py-1')
+    } else {
+        navbar.classList.add('py-1')
+        navbar.classList.remove('shadow', 'py-2')
+    }
+})
